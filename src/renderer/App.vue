438f5001ec03d439b240731fff061775eae3a042
Moustachauve <Moustachauve@users.noreply.github.com>
2017-07-14 13:18:35 -0400

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sticky-web'
+  }
+</script>
+
+<style>
+  /* CSS */
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic');
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+</style>
